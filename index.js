ad233e2ef6663527e515bef364442dc4dcb8c280
Jatin Poriya <111553605+InterestingPlus@users.noreply.github.com>
2025-04-07 10:06:07 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,95 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+
+const uri =
+  "mongodb+srv://jatin:nullclass@twiller.xooswpu.mongodb.net/?retryWrites=true&w=majority&appName=Twiller";
+const port = 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const postCollection = client.db("databse").collection("posts");
+    const userCollection = client.db("databse").collection("users");
+
+    app.post("/register", async (req, res) => {
+      const user = req.body;
+
+      const result = await userCollection.insertOne(user);
+
+      console.log("REgister : ", result);
+
+      res.send(result);
+    });
+
+    app.get("/loggedinuser", async (req, res) => {
+      const email = req.query.email;
+
+      const user = await userCollection.findOne({ email: email });
+      res.send(user);
+    });
+
+    app.post("/post", async (req, res) => {
+      const post = req.body;
+
+      const result = await postCollection.insertOne(post);
+      res.send(result);
+    });
+
+    app.get("/post", async (req, res) => {
+      const post = (await postCollection.find().toArray()).reverse();
+
+      res.send(post);
+    });
+
+    app.get("/userpost", async (req, res) => {
+      const email = req.query.email;
+
+      const post = (
+        await postCollection.find({ email: email }).toArray()
+      ).reverse();
+      res.send(post);
+    });
+
+    app.get("/user", async (req, res) => {
+      const user = await userCollection.find().toArray();
+      res.send(user);
+    });
+
+    app.patch("/userupdate/:email", async (req, res) => {
+      const filter = req.params;
+      const profile = req.body;
+
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: profile,
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+
+      res.send(result);
+    });
+
+    console.log("Database Connected Successfully!");
+  } catch (error) {
+    console.log("Database Error: ", error);
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Twiller Server is Working!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is Running on PORT ${port}`);
+});
